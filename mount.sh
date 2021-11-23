04cbd06190f3c5d1c79ae4ab2f221f18f3ceb57d
hhamza <hhamza@student.1337.ma>
2021-11-23 11:30:02 +0100

    Fixed mount.sh (Creating /var/log)

--- mount.sh
+++ mount.sh
@@ -5,11 +5,8 @@ LV_PREFIX=/dev/mapper/${VG}
 LV_NAMES=(home var srv tmp var--log)
 LV_MOUNTPOINTS=(/home /var /srv /tmp /var/log)
 
-if [ ! -d "/var/log" ]; then
-	mkdir "/var/log"
-fi
-
 for i in ${!LV_NAMES[@]}
 do
+	[ "${LV_NAMES[$i]}" = "var--log" ] && mkdir -p "/var/log"
 	mount "${LV_PREFIX}-${LV_NAMES[$i]}" "${LV_MOUNTPOINTS[$i]}"
 done
